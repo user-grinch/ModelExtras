507d3e73cb1bd0861a66a2c8d353604fcdb4bbd6
user-grinch <user.grinch@gmail.com>
2025-03-15 02:09:49 +0600

    Fix shadows going under ground

--- src/features/vehicle/avs/common.cpp
+++ src/features/vehicle/avs/common.cpp
@@ -1,26 +1,33 @@
 #include "pch.h"
 #include "common.h"
+#include "defines.h"
+#include <CCamera.h>
 #include <CCoronas.h>
 #include <CShadows.h>
-#include <CCamera.h>
-#include "defines.h"
+#include <CWorld.h>
 
-void Common::RegisterCorona(CVehicle* pVeh, int coronaID, CVector pos, uchar red, uchar green, uchar blue, uchar alpha, float size) {
-    if (!gConfig.ReadBoolean("VEHICLE_FEATURES", "LightCoronas", false)) {
+void Common::RegisterCorona(CVehicle *pVeh, int coronaID, CVector pos, uchar red, uchar green, uchar blue, uchar alpha, float size)
+{
+    if (!gConfig.ReadBoolean("VEHICLE_FEATURES", "LightCoronas", false))
+    {
         return;
     }
 
     CCoronas::RegisterCorona(coronaID, pVeh, red, green, blue, alpha, pos,
-        size * CORONA_SZ_MUL, 260.0f, CORONATYPE_SHINYSTAR, FLARETYPE_NONE, false, false, 0, 0.0f, false, 0.3f, 0, 30.0f, false, false);
+                             size * CORONA_SZ_MUL, 260.0f, CORONATYPE_SHINYSTAR, FLARETYPE_NONE, false, false, 0, 0.0f, false, 0.3f, 0, 30.0f, false, false);
 };
 
-float Common::NormalizeAngle(float angle) {
-    while (angle < 0.0f) angle += 360.0f;
-    while (angle >= 360.0f) angle -= 360.0f;
+float Common::NormalizeAngle(float angle)
+{
+    while (angle < 0.0f)
+        angle += 360.0f;
+    while (angle >= 360.0f)
+        angle -= 360.0f;
     return angle;
 }
 
-void Common::RegisterCoronaWithAngle(CVehicle* pVeh, int coronaID, CVector posn, uchar red, uchar green, uchar blue, uchar alpha, float angle, float radius, float size) {
+void Common::RegisterCoronaWithAngle(CVehicle *pVeh, int coronaID, CVector posn, uchar red, uchar green, uchar blue, uchar alpha, float angle, float radius, float size)
+{
     constexpr float RAD_TO_DEG = 180.0f / 3.141592653589793f;
 
     float vehicleAngle = NormalizeAngle(pVeh->GetHeading() * RAD_TO_DEG);
@@ -33,17 +40,19 @@ void Common::RegisterCoronaWithAngle(CVehicle* pVeh, int coronaID, CVector posn,
 
     float diameter = (radius / 2.0f);
 
-    if (differenceAngle < diameter || differenceAngle >(360.0f - diameter))
+    if (differenceAngle < diameter || differenceAngle > (360.0f - diameter))
         return;
 
     float alphaFloat = static_cast<float>(alpha);
 
-    if (differenceAngle < diameter + InertiaAngle) {
+    if (differenceAngle < diameter + InertiaAngle)
+    {
         float adjustedAngle = diameter - differenceAngle;
         float multiplier = adjustedAngle / InertiaAngle;
         alpha = static_cast<uchar>(std::clamp(alphaFloat * multiplier, 0.0f, 255.0f));
     }
-    else if (differenceAngle > (360.0f - diameter) - InertiaAngle) {
+    else if (differenceAngle > (360.0f - diameter) - InertiaAngle)
+    {
         float adjustedAngle = InertiaAngle - (differenceAngle - ((360.0f - diameter) - InertiaAngle));
         float multiplier = adjustedAngle / InertiaAngle;
         alpha = static_cast<uchar>(std::clamp(alphaFloat * multiplier, 0.0f, 255.0f));
@@ -52,7 +61,8 @@ void Common::RegisterCoronaWithAngle(CVehicle* pVeh, int coronaID, CVector posn,
     RegisterCorona(pVeh, coronaID, posn, red, green, blue, alpha, size);
 }
 
-RwTexture* Common::GetTexture(std::string texture) {
+RwTexture *Common::GetTexture(std::string texture)
+{
     if (Textures.contains(texture) && Textures[texture])
         return Textures[texture];
 
@@ -60,18 +70,19 @@ RwTexture* Common::GetTexture(std::string texture) {
     return Textures[texture];
 };
 
-void Common::RegisterShadow(CVehicle* pVeh, CVector position, unsigned char red, unsigned char green, unsigned char blue, unsigned int alpha, float angle, float currentAngle, const std::string& shadwTexName, float shdwSz, float shdwOffset, RwTexture* pTexture) {
-    if (shdwSz == 0.0f || !gConfig.ReadBoolean("VEHICLE_FEATURES", "LightShadows", false)) {
+void Common::RegisterShadow(CVehicle *pVeh, CVector position, unsigned char red, unsigned char green, unsigned char blue, unsigned int alpha, float angle, float currentAngle, const std::string &shadwTexName, float shdwSz, float shdwOffset, RwTexture *pTexture)
+{
+    if (shdwSz == 0.0f || !gConfig.ReadBoolean("VEHICLE_FEATURES", "LightShadows", false))
+    {
         return;
     }
 
+    float ground = CWorld::FindGroundZFor3DCoord(position.x, position.y, position.z, nullptr, nullptr);
     CVector center = pVeh->TransformFromObjectSpace(
         CVector(
             position.x + (shdwOffset * cos((90.0f - angle + currentAngle) * 3.14f / 180.0f)),
             position.y + ((1.2f + shdwOffset) * sin((90.0f - angle + currentAngle) * 3.14f / 180.0f)),
-            position.z
-        )
-    );
+            ground - 0.5f));
 
     float fAngle = pVeh->GetHeading() + (((angle + currentAngle) + 180.0f) * 3.14f / 180.0f);
 
@@ -81,8 +92,8 @@ void Common::RegisterShadow(CVehicle* pVeh, CVector position, unsigned char red,
     right *= shdwSz;
 
     CShadows::StoreShadowToBeRendered(2, (pTexture != NULL ? pTexture : Common::GetTexture(shadwTexName)), &center,
-        up.x, up.y,
-        right.x, right.y,
-        alpha, red, green, blue,
-        2.0f, false, 1.0f, 0, true);
+                                      up.x, up.y,
+                                      right.x, right.y,
+                                      alpha, red, green, blue,
+                                      2.0f, false, 1.0f, 0, true);
 };
