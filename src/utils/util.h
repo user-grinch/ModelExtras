f75d5a808eaf9771fa8ae9762b35c20e89c67ca4
Caner Karaca <canerkaraca_23@hotmail.com>
2026-08-23 18:39:19 +0300

    fix(lights): check component damage status via IsFrameDamaged so damaged parts immediately disable their coronas and shadows

--- src/utils/util.h
+++ src/utils/util.h
@@ -15,6 +15,7 @@ public:
   static bool IsDoorDamaged(CVehicle *pVeh, eDoors door);
   static bool IsLightDamaged(CVehicle *pVeh, eLights light);
   static bool IsPanelDamaged(CVehicle *pVeh, ePanels panel);
+  static bool IsFrameDamaged(CVehicle *pVeh, RwFrame *frame);
   static CVector UpdateRelativeToBoundingBox(CVehicle *pVeh, eDummyPos dummyPos, CVector center, CVector up, CVector right);
 
   static float NormalizeAngle(float angle);
