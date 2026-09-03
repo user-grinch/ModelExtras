4cf69bc8b1f2eedea7e67f762d1bd34a74743ea0
Caner Karaca <37447503+CanerKaraca23@users.noreply.github.com>
2026-09-03 23:55:57 +0300

    fix(events): fix crash on motorbike explosion by removing invalid prerender hook (#297)

--- src/utils/meevents.h
+++ src/utils/meevents.h
@@ -9,5 +9,7 @@ namespace MEEvents
     // vehicle
     static inline ThiscallEvent<AddressList<0x5343B2, H_CALL>, PRIORITY_AFTER, ArgPickN<CVehicle *, 0>, void(CVehicle *)> heliRenderEvent;
     static inline ThiscallEvent<AddressList<0x6D0E89, H_JUMP>, PRIORITY_BEFORE, ArgPickN<CVehicle *, 0>, void(CVehicle *)> vehRenderEvent;
-    static inline ThiscallEvent<AddressList<0x6AAB71, H_CALL, 0x6BEA36, H_CALL>, PRIORITY_BEFORE, ArgPickN<CVehicle *, 0>, void(CVehicle *)> vehPreRenderEvent;
+    // CAutomobile::PreRender only. Bikes go through the processScriptsEvent loop in each
+    // feature instead, CBike::PreRender has no equivalent call site to hook here.
+    static inline ThiscallEvent<AddressList<0x6AAB71, H_CALL>, PRIORITY_BEFORE, ArgPickN<CVehicle *, 0>, void(CVehicle *)> vehPreRenderEvent;
 }
