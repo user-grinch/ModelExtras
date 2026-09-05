417267248fa1a0a370dc230df20802c8e6e4eb6a
Grinch_ <user.grinch@gmail.com>
2026-09-05 10:44:04 +0600

    update clangd files

--- src/utils/worldutil.cpp
+++ src/utils/worldutil.cpp
@@ -3,11 +3,12 @@
 #include <CClock.h>
 #include <CWeather.h>
 
-bool WorldUtil::IsNightTime() {
-    uint8_t hours = CClock::ms_nGameClockHours;
-    return (hours >= 21 || hours < 6);
+bool WorldUtil::IsNightTime()
+{
+    return CClock::GetIsTimeInRange(20, 6);
 }
 
-bool WorldUtil::IsFoggy() {
-    return (CWeather::WeatherTypeInList == WEATHER_FOGGY_SF || CWeather::ForcedWeatherType == WEATHER_FOGGY_SF);
+bool WorldUtil::IsFoggy()
+{
+    return (CWeather::Foggyness > 0.1f) || (CWeather::Rain > 0.3f) || (CWeather::NewWeatherType == WEATHER_FOGGY_SF || CWeather::NewWeatherType == WEATHER_SANDSTORM_DESERT || CWeather::OldWeatherType == WEATHER_FOGGY_SF || CWeather::OldWeatherType == WEATHER_SANDSTORM_DESERT);
 }
