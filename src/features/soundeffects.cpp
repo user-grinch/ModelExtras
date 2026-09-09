8b7fb494402af64e77d4932a3570d4d0c6d53540
Caner Karaca <37447503+CanerKaraca23@users.noreply.github.com>
2026-09-09 07:41:44 +0300

    feat(sound): expand big vehicle models table and support mtruck subclass (#313)

--- src/features/soundeffects.cpp
+++ src/features/soundeffects.cpp
@@ -82,9 +82,10 @@ void SoundEffects::ProcessVehicle(CVehicle *pVeh)
             }
 
             int animGroup = pVeh->m_pHandlingData ? pVeh->m_pHandlingData->m_nAnimGroup : 0;
-            bool isAllowed = pVeh->m_nVehicleSubClass == VEHICLE_AUTOMOBILE &&
+            bool isAllowed = (pVeh->m_nVehicleSubClass == VEHICLE_AUTOMOBILE || pVeh->m_nVehicleSubClass == VEHICLE_MTRUCK) &&
                             (animGroup == ANIMGROUP_TRUCK || animGroup == ANIMGROUP_BUS || animGroup == ANIMGROUP_COACH ||
-                             pVeh->bIsBig || pVeh->bIsBus || pVeh->bIsVan || (pVeh->m_pHandlingData && pVeh->m_pHandlingData->m_fMass >= 3500.0f));
+                             pVeh->bIsBig || pVeh->bIsBus || (pVeh->bIsVan && pVeh->m_pHandlingData && pVeh->m_pHandlingData->m_fMass >= 2500.0f) ||
+                             (pVeh->m_pHandlingData && pVeh->m_pHandlingData->m_fMass >= 3500.0f));
             bool isBigVeh = isAllowed || std::find(ValidForReverseSound.begin(), ValidForReverseSound.end(), pVeh->m_nModelIndex) != ValidForReverseSound.end();
 
             if (bEngineSounds)
