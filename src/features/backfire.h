1e36a8277ddcc5e453abe478c01d77e0e5766dd3
Caner Karaca <37447503+CanerKaraca23@users.noreply.github.com>
2026-09-14 08:40:07 +0300

    feat(backfire): complete overhaul with Soundize/VehFuncs integration, realistic ALS and dynamic shift flames (#302)

--- src/features/backfire.h
+++ src/features/backfire.h
@@ -1,15 +1,25 @@
 #pragma once
 #include <plugin.h>
 #include "core/base.h"
+#include <Fx_c.h>
+#include <vector>
 
 struct BackfireData
 {
     bool wasFullThrottled = false;
     int m_nleftFires = 0;
     size_t prevTimer = 0;
+    int lastSoundizeGear = 0;
+
+    bool bSystemsInitialized = false;
+    std::vector<FxSystem_c *> m_fxLow;
+    std::vector<FxSystem_c *> m_fxHigh;
+    std::vector<RwMatrix *> m_matrices;
+
+    void CleanUpSystems();
 
     BackfireData(CVehicle *pVeh) {}
-    ~BackfireData() {}
+    ~BackfireData();
 };
 
 class BackFireEffect : public CVehFeature<BackfireData>
@@ -18,9 +28,9 @@ protected:
   void Init() override;
   void ReloadConfig() override;
   void Reload(CVehicle *pVeh) override { ReloadConfig(); }
-  static void BackFireFX(CVehicle *pVeh, float x, float y, float z, float dirX = 0.0f, float dirY = -25.0f, float dirZ = 0.0f);
-  static void BackFireSingle(CVehicle *pVeh);
-  static void BackFireMulti(CVehicle *pVeh);
+  static void EnsureSystemsCreated(CVehicle *pVeh, BackfireData &data);
+  static void BackFireSingle(CVehicle *pVeh, bool bPlaySound = true);
+  static void BackFireMulti(CVehicle *pVeh, bool bPlaySound = true);
   static void Process(CVehicle *pVeh);
 
 public:
