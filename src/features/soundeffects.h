82d14737c1a0e968d0a807029560d2e3183dc318
Caner Karaca <canerkaraca_23@hotmail.com>
2026-09-16 17:10:01 +0300

    feat(sound): add modern 3D car door open chime warning sound effect

--- src/features/soundeffects.h
+++ src/features/soundeffects.h
@@ -13,8 +13,15 @@ struct SoundEffectsData
     unsigned int m_nLastReverseSoundTime = 0;
     float m_fBrakePressure = 0.0f;
     float m_fMaxPedal = 0.0f;
+    StreamHandle m_hDoorChimeStream = 0;
     SoundEffectsData(CVehicle *pVeh) {}
-    ~SoundEffectsData() {}
+    ~SoundEffectsData()
+    {
+        if (m_hDoorChimeStream)
+        {
+            AudioMgr::StopLoopStream(m_hDoorChimeStream);
+        }
+    }
 };
 
 class SoundEffects : public CVehFeature<SoundEffectsData>
