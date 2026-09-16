4fc9262a7ef5b81faba189ed71bea5826dec3f05
Caner Karaca <canerkaraca_23@hotmail.com>
2026-09-16 18:57:27 +0300

    feat(sound): add realistic 3D brake pad friction squeak sound on braking

--- src/features/soundeffects.h
+++ src/features/soundeffects.h
@@ -13,8 +13,16 @@ struct SoundEffectsData
     unsigned int m_nLastReverseSoundTime = 0;
     float m_fBrakePressure = 0.0f;
     float m_fMaxPedal = 0.0f;
+    StreamHandle m_hBrakePadStream = 0;
+    float m_fBrakePadVol = 0.0f;
     SoundEffectsData(CVehicle *pVeh) {}
-    ~SoundEffectsData() {}
+    ~SoundEffectsData()
+    {
+        if (m_hBrakePadStream)
+        {
+            AudioMgr::StopLoopStream(m_hBrakePadStream);
+        }
+    }
 };
 
 class SoundEffects : public CVehFeature<SoundEffectsData>
